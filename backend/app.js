2849ccf80e4a3528595e6337f42def1dd7d945a3
DAVID <dpunzel15@georgefox.edu>
2020-04-02 12:43:05 -0700

    improving server.js - fetching initial post

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    { id: 'randomString123',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    { id: 'randomString456',
+      title: 'Second server-side post',
+      content: 'Also is coming from the server'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
