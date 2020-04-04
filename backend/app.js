5a185fdfd1eb654ea5d4c1414e6a031d1e218e7e
DAVID <dpunzel15@georgefox.edu>
2020-04-03 21:47:37 -0700

    Creating a post instance

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+const Post = require('./models/post');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,10 +21,12 @@ app.use((req, res, next) => {
   );
   next();
 });
-// gdSaLej8PXXd3cCI
-
+/* gdSaLej8PXXd3cCI */
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
