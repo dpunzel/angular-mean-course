1dad204e541bbd9a6f57a353ac5974e67ae94952
DAVID <dpunzel15@georgefox.edu>
2020-04-04 19:57:07 -0700

    node express app to MongoDB setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://David:gdSaLej8PXXd3cCI@cluster0-zyk2v.mongodb.net/test?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
