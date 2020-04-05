48be8eedba7c071aa92f4c60fff7ddf6e8aba406
DAVID <dpunzel15@georgefox.edu>
2020-04-04 21:25:34 -0700

    Fetching data from database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://David:gdSaLej8PXXd3cCI@cluster0-zyk2v.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://David:gdSaLej8PXXd3cCI@cluster0-zyk2v.mongodb.net/node-angular?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to database');
   })
@@ -35,29 +35,21 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: documents
+      });
+    });
+
 });
 
 module.exports = app;
